2011bba01b9f3d60944cdafcf6fddfa79c6db9f0
Juan Soto <juan@juansoto.me>
2016-07-21 10:44:06 -0400

    Fix timeout bug
    Previously, the program would wait the full 3500ms (the timeout max) before finishing, even when the server was up. Now it should only take that long if the
    request actually times out.

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 'use strict'
 
 const fetch = require('node-fetch')
-const delay = require('delay')
+const timeout = require('promise.timeout')
 const time = require('promise-time')
 
 const url = 'https://pgorelease.nianticlabs.com/plfe/'
-// Resolve after 3500 seconds with the value `3500`
-const timeout = delay(3500, 3500)
 
-// Returns a promise that resolves to the time a `fetch` to `url` took
+// Returns a promise that resolves to the time a `fetch` to `url` took or `-1`
+// on any error
 function getfetchPromise () {
   const timePromise = time(fetch)(url)
 
-  return timePromise.then(() => timePromise.time)
+  return timePromise.then(() => timePromise.time).catch(() => -1)
 }
 
-// Returns the return value of `getfetchPromise` or `timeout`, whichever
-// resolves first
+// Runs `getfetchPromise` but resolve with `-1` if it takes longer than 3500ms
 function getTimePromise () {
-  return Promise.race([getfetchPromise(), timeout])
+  return timeout(getfetchPromise, 3500, true)().catch(() => -1)
 }
 
+// Returns an appropiate string based on the time given
 function judge (time) {
   if (time === -1) return 'Error! Probably not a good sign, but try again.'
   if (time < 800) return 'Yep. Go outside and catch some!'
@@ -28,10 +27,15 @@ function judge (time) {
   if (time >= 3000) return 'Nope, servers are down! Go back to work.'
 }
 
+// Helper function to abstract calling `fn` on `promise` resolution and error
+function always (promise, fn) {
+  return promise.then(fn, fn)
+}
+
 function isPokemonGoUp () {
   const p = getTimePromise()
 
-  return p.then(judge).catch(() => judge(-1))
+  return always(p, judge)
 }
 
 module.exports = isPokemonGoUp
